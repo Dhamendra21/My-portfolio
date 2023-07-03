316702628dcda241706e8e3a98384959bc9049cd
Dhamendra21 <126798740+Dhamendra21@users.noreply.github.com>
2023-07-03 14:19:52 +0530

    Update Nav.js

--- src/components/pages/Nav.js
+++ src/components/pages/Nav.js
@@ -5,7 +5,7 @@ function Nav (){
 
        <nav className="m-5 " >
        <ul class="nav flex-md-column  " className="navigation" >
-        <h1 className="text-center">D</h1>
+        <h1 className="text-center">DH</h1>
   <li class="nav-item m-2">
     <Link class="nav-link link " aria-current="page" to="/">Home</Link>
   </li>
